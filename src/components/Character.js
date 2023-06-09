6628455cf31c32848726f2ebe62e2894d0d0a882
Madhu Nyoupane <nyoupanemadhu07@gmail.com>
2023-06-09 16:05:51 -0700

    feat: add comment implement

--- src/components/Character.js
+++ src/components/Character.js
@@ -11,12 +11,38 @@ const Character = (props) => {
   const [stamina, setStamina] = useState(props.stamina);
   //Gold
   const [gold, setGold] = useState(props.gold);
+  //Value of comment to display
+  const [commentOnDisplay, setCommentOnDisplay] = useState(props.comment);
+  //comment visibility
+  const [isOpen, setIsOpen] = useState(false);
+  //content of comment input field
+  const [comment, setComment] = useState("");
 
   const fetchPlayer = async () => {
     const res = await fetch(`http://localhost:5000/players/${props.id}`);
     const data = await res.json();
     console.log(data);
     return data;
+  };
+
+  const alterComment = () => {
+    setIsOpen(true);
+  };
+
+  const submitComment = async () => {
+    setIsOpen(false);
+    setComment("");
+    setCommentOnDisplay(comment);
+    let updatedPlayer = await fetchPlayer();
+    updatedPlayer.comment = comment;
+    await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updatedPlayer),
+    });
+    //window.location.reload(false);
   };
   return (
     <div>
@@ -28,82 +54,110 @@ const Character = (props) => {
         Stamina at {stamina}
       </p>
       <p>Gold: {gold}</p>
-      <p className={props.comment ? "visible" : "hidden"}>
-        Comment: {props.comment}
+      <p className={commentOnDisplay ? "visible" : "hidden"}>
+        Comment: {commentOnDisplay}
       </p>
       <button
-        onClick={async () => {
-          let updatedPlayer = await fetchPlayer();
-          updatedPlayer.health = parseInt(updatedPlayer.health) + 10;
-          updatedPlayer.gold = parseInt(updatedPlayer.gold) - 2;
-          console.log(updatedPlayer);
-          setHealth(parseInt(health) + 10);
-          setGold(parseInt(gold) - 2);
-
-          await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify(updatedPlayer),
-          });
+        onClick={() => {
+          alterComment();
         }}
       >
-        Add 10 Health (Costs 2 Gold)
-      </button>
-      <br />
-      <button
-        onClick={async () => {
-          let updatedPlayer = await fetchPlayer();
-          updatedPlayer.stamina = parseInt(updatedPlayer.stamina) + 5;
-          updatedPlayer.gold = parseInt(updatedPlayer.gold) - 1;
-
-          setStamina(parseInt(stamina) + 5);
-          setGold(parseInt(gold) - 1);
-
-          await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify(updatedPlayer),
-          });
-        }}
-      >
-        Add 5 Stamina (Costs 1 Gold)
-      </button>
-      <br />
-      <button
-        onClick={async () => {
-          let updatedPlayer = await fetchPlayer();
-          updatedPlayer.stamina = parseInt(updatedPlayer.stamina) - 5;
-          updatedPlayer.gold = parseInt(updatedPlayer.gold) + 3;
-          updatedPlayer.health = parseInt(updatedPlayer.health) - 10;
-
-          setGold(parseInt(gold) + 3);
-          setHealth(parseInt(health) - 10);
-          setStamina(parseInt(stamina) - 5);
-
-          await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
-            method: "PUT",
-            headers: {
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify(updatedPlayer),
-          });
-        }}
-      >
-        Add 3 Gold (Costs 10 Health and 5 Stamina)
+        {commentOnDisplay ? "Change Comment" : "Add Comment"}
       </button>
       <br />
 
-      <label htmlFor="nameChange">Change Player's Name:</label>
-      <br />
-      <input
-        type="text"
-        id="nameChange"
-        onChange={(e) => setName(e.target.value)}
-      />
+      {isOpen && (
+        <div>
+          <input
+            type="text"
+            value={comment}
+            onChange={(e) => {
+              setComment(e.target.value);
+            }}
+          />
+          <button onClick={() => submitComment()}>Submit</button>
+        </div>
+      )}
+
+      {!isOpen && (
+        <div>
+          {" "}
+          <button
+            onClick={async () => {
+              let updatedPlayer = await fetchPlayer();
+              updatedPlayer.health = parseInt(updatedPlayer.health) + 10;
+              updatedPlayer.gold = parseInt(updatedPlayer.gold) - 2;
+              console.log(updatedPlayer);
+              setHealth(parseInt(health) + 10);
+              setGold(parseInt(gold) - 2);
+
+              await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
+                method: "PUT",
+                headers: {
+                  "Content-type": "application/json",
+                },
+                body: JSON.stringify(updatedPlayer),
+              });
+            }}
+          >
+            Add 10 Health (Costs 2 Gold)
+          </button>
+          <br />
+          <button
+            onClick={async () => {
+              let updatedPlayer = await fetchPlayer();
+              updatedPlayer.stamina = parseInt(updatedPlayer.stamina) + 5;
+              updatedPlayer.gold = parseInt(updatedPlayer.gold) - 1;
+
+              setStamina(parseInt(stamina) + 5);
+              setGold(parseInt(gold) - 1);
+
+              await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
+                method: "PUT",
+                headers: {
+                  "Content-type": "application/json",
+                },
+                body: JSON.stringify(updatedPlayer),
+              });
+            }}
+          >
+            Add 5 Stamina (Costs 1 Gold)
+          </button>
+          <br />
+          <button
+            onClick={async () => {
+              let updatedPlayer = await fetchPlayer();
+              updatedPlayer.stamina = parseInt(updatedPlayer.stamina) - 5;
+              updatedPlayer.gold = parseInt(updatedPlayer.gold) + 3;
+              updatedPlayer.health = parseInt(updatedPlayer.health) - 10;
+
+              setGold(parseInt(gold) + 3);
+              setHealth(parseInt(health) - 10);
+              setStamina(parseInt(stamina) - 5);
+
+              await fetch(`http://localhost:5000/players/${updatedPlayer.id}`, {
+                method: "PUT",
+                headers: {
+                  "Content-type": "application/json",
+                },
+                body: JSON.stringify(updatedPlayer),
+              });
+            }}
+          >
+            Add 3 Gold (Costs 10 Health and 5 Stamina)
+          </button>
+          <br />
+          <label htmlFor="nameChange">Change Player's Name:</label>
+          <br />
+          <input
+            type="text"
+            id="nameChange"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />{" "}
+        </div>
+      )}
       <br />
       <br />
       <hr />
